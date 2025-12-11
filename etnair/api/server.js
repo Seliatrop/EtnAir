49f811e27b73c297afb7fffdcc223b662124f6a5
Your Name <you@example.com>
2025-12-11 16:57:52 +0100

    JWT + Bcrypt dans l'API

--- etnair/api/server.js
+++ etnair/api/server.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const { PrismaClient } = require('@prisma/client');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 const prisma = new PrismaClient();
 const port = 3000;
+const sel = 10;
 
 app.use(express.json());
 
@@ -54,9 +57,11 @@ app.post('/utilisateurs', async (req, res) => {
 app.post('/auth/register', async (req, res) => {
   const { email, nom, password } = req.body;
   try {
+    const hashPassword = await bcrypt.hash(password, sel);
     const user = await prisma.utilisateur.create({
-      data: { email, nom, password },
+      data: { email, nom, hashPassword },
     });
+    console.log(user);
     res.status(201).json(user);
   } catch (error) {
     res.status(400).json({ error: "Email déjà pris ou données invalides" });
@@ -66,13 +71,15 @@ app.post('/auth/register', async (req, res) => {
 app.post('/auth/login', async (req, res) => {
   const { email, password } = req.body;
   try {
+    const egale = await bcrypt.compare(password, hashPassword);
     const user = await prisma.utilisateur.findUnique({
       where: { email },
     });
-    if (!user || user.password !== password) {
+    if (!user || !egale) {
       return res.status(401).json({ error: "Identifiants invalides" });
     }
-    res.json({ message: "Connexion réussie", user });
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.json({ message: "Connexion réussie", user, token });
   } catch (error) {
     res.status(500).json({ error: "Erreur serveur" });
   }
