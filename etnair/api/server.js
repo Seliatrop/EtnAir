20609c538d3b51629c99f602fca50a9629580b28
Your Name <you@example.com>
2025-12-12 09:11:03 +0100

    Prettier* fix ""

--- etnair/api/server.js
+++ etnair/api/server.js
@@ -1,8 +1,8 @@
-require('dotenv').config();
-const express = require('express');
-const { PrismaClient } = require('@prisma/client');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+require("dotenv").config();
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 const prisma = new PrismaClient();
@@ -14,7 +14,7 @@ app.use(express.json());
 // --- ROUTES ---
 
 // 1. Récupérer toutes les annonces
-app.get('/annonces', async (req, res) => {
+app.get("/annonces", async (req, res) => {
   try {
     const annonces = await prisma.annonce.findMany();
     res.json(annonces);
@@ -24,7 +24,7 @@ app.get('/annonces', async (req, res) => {
 });
 
 // 2. Créer une annonce
-app.post('/annonces', async (req, res) => {
+app.post("/annonces", async (req, res) => {
   const { titre, description, prix, utilisateurId } = req.body;
   try {
     const result = await prisma.annonce.create({
@@ -37,12 +37,14 @@ app.post('/annonces', async (req, res) => {
     });
     res.status(201).json(result);
   } catch (error) {
-    res.status(400).json({ error: "Erreur création (Vérifiez que l'utilisateur existe)" });
+    res
+      .status(400)
+      .json({ error: "Erreur création (Vérifiez que l'utilisateur existe)" });
   }
 });
 
 // 3. Créer un utilisateur (Pour pouvoir créer des annonces ensuite)
-app.post('/utilisateurs', async (req, res) => {
+app.post("/utilisateurs", async (req, res) => {
   const { email, nom, password } = req.body;
   try {
     const user = await prisma.utilisateur.create({
@@ -54,7 +56,7 @@ app.post('/utilisateurs', async (req, res) => {
   }
 });
 
-app.post('/auth/register', async (req, res) => {
+app.post("/auth/register", async (req, res) => {
   const { email, nom, password } = req.body;
   try {
     const hashPassword = await bcrypt.hash(password, sel);
@@ -68,7 +70,7 @@ app.post('/auth/register', async (req, res) => {
   }
 });
 
-app.post('/auth/login', async (req, res) => {
+app.post("/auth/login", async (req, res) => {
   const { email, password } = req.body;
   try {
     const egale = await bcrypt.compare(password, hashPassword);
@@ -78,14 +80,16 @@ app.post('/auth/login', async (req, res) => {
     if (!user || !egale) {
       return res.status(401).json({ error: "Identifiants invalides" });
     }
-    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-    res.json({ message: "Connexion réussie", user, token });
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.json({ message: "Connexion réussie", user });
   } catch (error) {
     res.status(500).json({ error: "Erreur serveur" });
   }
 });
 
-app.get('/test', (req, res) => {
+app.get("/test", (req, res) => {
   res.json({ message: "Test réussi", status: "ok" });
 });
 
